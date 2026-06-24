8f6b21bc18e48fe7090ca14f23305861edf618be
lucidreamsomnia <ignanantasatyap@gmail.com>
2026-06-24 14:22:37 +0800

    buat dashboard dosen (awal)

--- frontend/js/dashboard.js
+++ frontend/js/dashboard.js
@@ -1,1 +1,54 @@
-// TODO
+// Pie Chart Kelulusan
+
+new Chart(
+    document.getElementById("kelulusanChart"),
+    {
+        type: "pie",
+
+        data: {
+            labels: [
+                "Lulus",
+                "Tidak Lulus"
+            ],
+
+            datasets: [{
+                data: [
+                    280,
+                    60
+                ]
+            }]
+        }
+    }
+);
+
+
+// Bar Chart Mata Kuliah
+
+new Chart(
+    document.getElementById("matkulChart"),
+    {
+        type: "bar",
+
+        data: {
+            labels: [
+                "Basis Data",
+                "PBW",
+                "Jaringan",
+                "AI",
+                "PPDM"
+            ],
+
+            datasets: [{
+                label: "Rata-rata Nilai",
+
+                data: [
+                    82,
+                    85,
+                    78,
+                    80,
+                    84
+                ]
+            }]
+        }
+    }
+);
